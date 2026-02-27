f4c8858f487189e9a4af641a6cc5b4fb1f789041
Truuuuuuuu <jethruel@gmail.com>
2026-02-27 21:43:43 +0800

    feat/mbr wip, refactor navbar, add botnavbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en" data-theme="light">
+<html lang="en" data-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -14,73 +14,87 @@
 <div>
     @if (!isset($hideNavbar))
         <nav class=" sticky top-0 z-50 bg-base-100 shadow flex justify-between items-center py-4 border-b border-white/10 p-5">
-            {{-- Logo --}}
-            <div>
-                <a href="/">
-                    <img src="{{ asset('images/logo-only.svg') }}" alt="" class="w-10 h-auto">
-                </a>
-            </div>
 
-            @auth
-                @role('tenant')
+
+                {{-- Logo --}}
+                <div class="flex-shrink-0 ">
+                    <a href="/">
+                        <img src="{{ asset('images/logo-only.svg') }}" alt="" class="w-10 h-auto" >
+                    </a>
+                </div>
+
+                @auth
+                    @role('tenant')
+                      {{--  <div class="flex-1 w-md border">
+                            <!-- Search -->
+                            <div class=" w-full max-w-xs lg:max-w-lg  text-base-content">
+                                @include('components.search-bar')
+                            </div>
+                        </div>--}}
+
+
+
                     {{-- links --}}
-                    <div class="text-base-content flex gap-10 font-semibold">
-                        <a href="">Browse</a>
-                        <a href="">My Home</a>
+                    <div class="hidden lg:flex text-base-content  gap-8 items-center whitespace-nowrap font-semibold ">
+                        <a href="">Explore</a>
+                        <a href="">My Unit</a>
+                        <a href="">Reservation</a>
                     </div>
 
-                    <!-- Search -->
-                    <div class="w-100 text-base-content">
-                        @include('components.search-bar')
-                    </div>
 
+                    @endrole
 
-
-                @endrole
-
-                @role('host')
+                    @role('host')
                     {{-- links --}}
-                    <div class="text-base-content flex gap-10 font-semibold">
+                    <div class="hidden lg:flex text-base-content  gap-10 font-semibold">
                         <a href="">Reservations</a>
                         <a href="">Listings</a>
 
                     </div>
-                @endrole
-                <div class="space-x-3 font-bold flex">
-                    <div class="btn btn-ghost btn-circle bg-gray-200" tabindex="0" role="button">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                            <path fill-rule="evenodd" d="M4.804 21.644A6.707 6.707 0 0 0 6 21.75a6.721 6.721 0 0 0 3.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 0 1-.814 1.686.75.75 0 0 0 .44 1.223ZM8.25 10.875a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25ZM10.875 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z" clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                    <div class="btn btn-ghost btn-circle bg-blue-200">
-                        <h1>{{Auth::user()->name[0]}}</h1>
-                    </div>
-                    <div class="dropdown dropdown-end" >
-                        <div class="btn btn-ghost btn-circle bg-gray-200" tabindex="0" role="button">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    @endrole
+                @endauth
 
-                            </svg>
+
+
+
+                @guest
+                    <div class="space-x-6 font-semibold ">
+                        <a href="/user-option" class="btn btn-primary">Get Started</a>
+                        <a href="/login" class="text-base-content">Sign in</a>
+                    </div>
+                @endguest
+
+                @auth
+                        <div class="flex space-x-3 font-bold ">
+                            <div class="flex btn btn-ghost btn-circle text-white lg:bg-gray-200 lg:text-black" tabindex="0" role="button">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-15 lg:size-6">
+                                    <path fill-rule="evenodd" d="M4.804 21.644A6.707 6.707 0 0 0 6 21.75a6.721 6.721 0 0 0 3.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 0 1-.814 1.686.75.75 0 0 0 .44 1.223ZM8.25 10.875a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25ZM10.875 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="hidden lg:flex btn btn-ghost btn-circle bg-blue-200">
+                                <h1>{{Auth::user()->name[0]}}</h1>
+                            </div>
+                            <div class="hidden lg:flex dropdown dropdown-end" >
+                                <div class="btn btn-ghost btn-circle bg-gray-200" tabindex="0" role="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+
+                                    </svg>
+                                </div>
+                                <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm font-normal">
+                                    <li><a >Settings</a></li>
+                                    <li><a><form method="POST" action="/logout">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="hover:cursor-pointer">Log Out</button>
+                                            </form>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm font-normal">
-                            <li><a >Settings</a></li>
-                            <li><a><form method="POST" action="/logout">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="hover:cursor-pointer">Log Out</button>
-                                    </form>
-                                </a></li>
-                        </ul>
-                    </div>
-                <div/>
-            @endauth
+                @endauth
 
-            @guest
-                <div class="space-x-6 font-semibold">
-                    <a href="/user-option" class="btn btn-primary">Get Started</a>
-                    <a href="/login" class="text-base-content">Sign in</a>
-                </div>
-            @endguest
         </nav>
     @endif
 
--- resources/views/tenant/homepage.blade.php
+++ resources/views/tenant/homepage.blade.php
@@ -1,36 +1,69 @@
 <x-layout>
     <x-slot:heading>Login Success</x-slot:heading>
     {{--Bottom navbar for MOBILE--}}
-    <div class="btm-nav md:hidden fixed bottom-0 left-0 right-0 z-50 bg-base-100 border-t">
-        <a href="/">
-            <svg class="h-5 w-5" ...>...</svg>
-            <span class="btm-nav-label text-xs">Home</span>
-        </a>
-        <a href="/search">
-            🔍
-            <span class="btm-nav-label text-xs">Search</span>
-        </a>
-        <a href="/favorites">
-            ❤️
-            <span class="btm-nav-label text-xs">Saved</span>
-        </a>
-        <a href="/profile">
-            👤
-            <span class="btm-nav-label text-xs">Profile</span>
-        </a>
-    </div>
+    <div class="btm-nav md:hidden fixed bottom-0 left-0 right-0 z-50 bg-base-100 border-t border-gray-300 h-20">
+        <div class="flex justify-between items-center  h-full px-8">
+            <div>
+                <a href="" class="flex flex-col items-center justify-center text-base-content/70">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
 
+                    <span class="btm-nav-label text-xs">Explore</span>
+                </a>
+            </div>
+
+            <div>
+                <a href="" class="flex flex-col items-center justify-center text-base-content/70">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
+                    </svg>
+
+                    <span class="btm-nav-label text-xs ">My Unit</span>
+                </a>
+            </div>
+
+            <div>
+                <a href="" class="flex flex-col justify-center items-center text-base-content/70">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z" />
+                    </svg>
+
+                    <span class="btm-nav-label text-xs">Reservation</span>
+                </a>
+            </div>
+
+            <div>
+                <a href="" class="flex flex-col justify-center items-center text-base-content/70">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+
+                    <span class="btm-nav-label text-xs">Profile</span>
+                </a>
+            </div>
+
+        </div>
+
+
+    </div>
+        <div class="flex  w-full justify-center mt-5 ">
+            <!-- Search -->
+            <div class=" w-full max-w-xs lg:max-w-lg  text-base-content">
+                @include('components.search-bar')
+            </div>
+        </div>
     {{--student meal--}}
     <section class="p-7 text-base-content">
-        <div class="flex items-center gap-2">
+        <div class="flex items-center gap-2 ">
             <h1 class="text-xl font-semibold">Affordable for students!</h1>
             <a href="#" class="btn btn-ghost btn-circle bg-gray-200 w-8 h-8">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  class="size-6">
                     <path fill-rule="evenodd" d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
                 </svg>
             </a>
         </div>
-        <div class="grid grid-cols-2 lg:grid-cols-4 gap-x-2 gap-y-1 lg:gap-x-4 lg:gap-y-4">
+        <div class="grid grid-cols-2 lg:grid-cols-4 gap-x-2 gap-y-1 lg:gap-x-4 lg:gap-y-4 mt-5">
             @forelse($listings as $listing)
                 <x-bhouse-card :$listing/>
             @empty
