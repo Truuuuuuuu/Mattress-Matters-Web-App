e3d4f4782f2da2223382d8698f991bc57b7909f1
Truuuuuuuu <jethruel@gmail.com>
2026-04-16 22:05:51 +0800

    commented out, no logic is changes just the UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,9 +8,9 @@
         <div class="flex items-center justify-center text-base-content">
             <div class="w-full lg:max-w-md mx-auto px-6">
                 <h2 class="text-center text-lg font-bold ">Log in or sign up</h2>
-                <x-divider class="bg-gray-300 mt-5"></x-divider>
-                <h1 class="font-semibold mt-5  text-xl ">Welcome to Mattress Matters</h1>
-                <x-forms.form method="POST" action="/login" class="mt-5" >
+                <x-divider class="bg-gray-300 my-3"></x-divider>
+                <h1 class="font-semibold text-xl ">Welcome to Mattress Matters</h1>
+                {{--<x-forms.form method="POST" action="/login" class="mt-5" >
                     <x-forms.input label="Email" name="email" type="email" class="rounded-xl input input-primary input-lg lg:input-md" placeholder="Email" />
                     <x-forms.error name="email"/>
                     <x-forms.input label="Password" name="password" type="password" class="rounded-xl input input-primary input-lg lg:input-md" placeholder="Password" />
@@ -18,19 +18,18 @@
                     <a href="#" class="block text-end -mt-5 hover:text-blue-700">Forgot Password?</a>
 
                     <x-forms.button class="btn btn-primary w-full btn-lg lg:btn-md ">Log in</x-forms.button>
-                </x-forms.form>
+                </x-forms.form>--}}
 
-                <x-divider class="bg-blue-900 my-10"></x-divider>
 
                 <div class="flex flex-col gap-2 mt-5">
                     <!-- Google -->
-                    <a href="{{route('social.redirect', 'google')}}" class="btn bg-white text-black border-black">
+                    <a href="{{route('social.redirect', 'google')}}" class="btn bg-white text-black border-black rounded-xl hover:bg-gray-100">
                         <svg aria-label="Google logo" width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                         Continue with Google
                     </a>
 
                     <!-- Facebook -->
-                    <a href="{{route('social.redirect', 'facebook')}}" class="btn bg-white border border-black text-gray-700 hover:bg-gray-100 gap-2">
+                    <a href="{{route('social.redirect', 'facebook')}}" class="btn bg-white border border-black rounded-xl text-gray-700 hover:bg-gray-100 gap-2">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24"
