9697edde819e76d712e14febdc54b7fe545cd7da
Truuuuuuuu <jethruel@gmail.com>
2026-04-25 17:51:25 +0800

    listings index host responsive

--- resources/views/host/listings.blade.php
+++ resources/views/host/listings.blade.php
@@ -1,33 +1,27 @@
 <x-layout>
     <x-slot:heading>Listings</x-slot:heading>
 
-    <div class="w-full max-w-7xl mx-auto  px-5">
-        <section class="py-10">
-            <div>
-                <div class="flex justify-between items-center ">
-                    <h1 class="text-3xl font-semibold">My Listings</h1>
-                    <a href="{{route('host.create')}}" class="btn btn-circle w-50 border border-black">
-                        <div>
-                            <x-lucide-plus class="w-6 h-6"/>
-                        </div>
-                        <span>Add Listing</span>
-                    </a>
-                </div>
+    <div class="w-full max-w-7xl mx-auto  px-5 py-10">
+        <div class="lg:px-12 xl:px-8">
+            <div class="flex justify-between items-center ">
+                <h1 class="text-3xl font-semibold">My Listings</h1>
+                <a href="{{route('host.create')}}" class="btn btn-circle w-50 border border-black">
+                    <div>
+                        <x-lucide-plus class="w-6 h-6"/>
+                    </div>
+                    <span>Add Listing</span>
+                </a>
             </div>
+        </div>
 
-            <div class="grid place-items-center mt-15   ">
-                <div class="w-full lg:max-w-6xl  grid grid-cols-4 gap-10">
-                    @forelse($listings as $listing)
-
-                        <x-bhouse-card :$listing width="92"/>
-                    @empty
-                        <p class="col-span-full text-center text-gray-500">
-                            No listings available.
-                        </p>
-                    @endforelse
-                </div>
-
-            </div>
-        </section>
+        <div class=" space-y-3 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-2 gap-y-1 lg:gap-x-2 lg:gap-y-6 mt-5 place-self-center">
+                @forelse($listings as $listing)
+                    <x-bhouse-card :$listing />
+                @empty
+                    <p class="col-span-full text-center text-gray-500">
+                        No listings available.
+                    </p>
+                @endforelse
+        </div>
     </div>
 </x-layout>
